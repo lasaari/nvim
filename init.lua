4b18808c27378d8c1951227ac2c19104a528c068
Lasse Saarinen <lasse.saarinen@kapsi.fi>
2022-05-21 07:02:39 +0300

    init

---
+++ init.lua
@@ -0,0 +1,19 @@
+-- nvim config based on chris@©machine's Neovim from scratch tutorials
+-- https://www.youtube.com/watch?v=ctH-a-1eUME&list=PLhoH5vyxr6Qq41NFL4GvhFp-WLd5xzIzZ
+
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.colorscheme"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comment"
+require "user.gitsigns"
+require "user.nvim-tree"
+require "user.bufferline"
+require "user.neovide"
+require "user.lualine"
+require "user.toggleterm"
